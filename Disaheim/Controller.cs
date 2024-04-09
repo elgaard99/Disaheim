52e21ae7f29fbb5d87db28c7b29400e71a81cc8e
Sander Elgaard Andersen <sean62359@hotmail.edu.ucl.dk>
2024-04-09 11:55:08 +0200

    UnitTest 3 gennemført

---
+++ Disaheim/Controller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Controller
+    {
+        List<Book> books;
+
+        public List<Book> Books {  get {  return books; } set { books = value; } }
+
+        List<Amulet> amulets;
+        public List<Amulet> Amulets { get { return amulets; } set { amulets = value; } }
+
+        public Controller()
+        { 
+            Books = new List<Book>();
+            Amulets = new List<Amulet>();
+        }
+
+        public void AddToList(Book book)
+        {
+            books.Add(book);
+        }
+
+        public void AddToList(Amulet amulet)
+        {
+            amulets.Add(amulet);
+        }
+    }
+}
---
+++ Disaheim/Utility.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Utility
+    {
+        public double GetValueOfBook(Book book)
+        {
+            return book.Price;
+        }
+
+        public double GetValueOfAmulet(Amulet amulet)
+        {
+            double amuletPrice = -1;
+
+            switch (amulet.Quality)
+            {
+                case Level.low:
+                    amuletPrice = 12.5;
+                    break;
+
+                case Level.medium:
+                    amuletPrice = 20;
+                    break;
+
+                case Level.high:
+                    amuletPrice = 27.5;
+                    break;
+            }
+
+            return amuletPrice;
+        }
+    }
+}
