1ab5ab614581495eff73b218ea1251d8a23dd726
Sander Elgaard Andersen <sean62359@hotmail.edu.ucl.dk>
2024-04-09 10:36:00 +0200

    Add project files.

---
+++ Disaheim/Amulet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Amulet
+    {
+        string itemId;
+        public string ItemId { get { return itemId; } set { itemId = value; } }
+
+        string design;
+        public string Design { get { return design; } set { design = value; } }
+
+        Level quality;
+        public Level Quality {  get { return quality; } set {  quality = value; } }
+        
+        public Amulet(string itemId)
+        {  
+            ItemId = itemId;
+        }
+
+        public Amulet(string itemId, Level quality) : this(itemId)
+        {
+            Quality = quality;
+        }
+
+        public Amulet(string itemId, Level quality, string design) : this(itemId, quality)
+        {
+            Design = design;
+        }
+    }
+}
---
+++ Disaheim/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Book
+    {
+        string itemId;
+        public string ItemId { get {  return itemId; } set { itemId = value; } }
+
+        string title;
+        public string Title { get { return title; } set { title = value; } }
+
+        double price;
+        public double Price { get { return price; } set { price = value; } }
+
+        public Book(string itemId)
+        {
+            ItemId = itemId;
+            this.title = "";
+            this.price = 0;
+        }
+
+        public Book(string itemId, string title) : this(itemId)
+        { 
+            Title = title;
+            this.price = 0;
+        }
+
+        public Book(string itemId, string title, double price) : this(itemId, title) 
+        { 
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"ItemId: 1, Title: , Price: 0";
+        }
+    }
+}
