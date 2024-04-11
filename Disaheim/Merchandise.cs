8d31bd2739914b57da1817d40bc5db7509d5697d
Sander Elgaard Andersen <sean62359@hotmail.edu.ucl.dk>
2024-04-11 10:03:47 +0200

    Merchandise.cs tilføjet

---
+++ Disaheim/Merchandise.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Merchandise
+    {
+        string itemId;
+        public string ItemId { get { return itemId; } set { itemId = value; } }
+
+        public Merchandise(string itemId)
+        {
+            ItemId = itemId;
+        }
+
+        public override string ToString()
+        {
+            return "ItemId: " + itemId;
+        }
+    }
+}
