3b6b4a9e9d337a609943e05d7f5459119e88370f
Sander Elgaard Andersen <sean62359@hotmail.edu.ucl.dk>
2024-04-11 10:17:39 +0200

    Course.cs tilføjet

---
+++ DisaheimTest/Course.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisaheimTest
+{
+    public class Course
+    {
+        string name;
+        public string Name { get { return name; } set { name = value; } }
+
+        int durationInMinutes;
+        public int DurationInMinutes { get {  return durationInMinutes; } set {  durationInMinutes = value; } }
+
+        public Course(string name) 
+        { 
+            Name = name;
+            durationInMinutes = 0;
+        }
+
+        public Course(string name, int durationInMinutes) : this(name) 
+        { 
+            DurationInMinutes = durationInMinutes;
+        }
+    }
+}
